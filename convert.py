e4d6e63eba42d81d4bdf6e196b02cc3ae2522feb
elvischido <96382131+elvischido@users.noreply.github.com>
2022-02-21 04:58:02 +1030

    Update convert.py

--- convert.py
+++ convert.py
@@ -20,20 +20,22 @@ parser.add_argument("-dd",
 parser.add_argument("-cw",
                     "--cls_wts",
                     help="a string containing the class weights.", type=str)
-parser.add_argument("-i",
-                    "--image_dir",
-                    help="Path to the folder where the input image files are stored.", type=str, default=None)
 parser.add_argument("-cm",
-                    "--cls_mg",
-                    help="a .csv file containing classes to be merged",
+                    "--cls_mp",
+                    help="a .json file containing classes to be mapped",
                     type=str, default=None)
 
 args = parser.parse_args()
 
 directory = args.img_dir
 data_directory = args.data_dir
-class_weights = args.cls_wts
-class_merge = args.cls_mg
+class_weights = json.loads(args.cls_wts)
+class_maps = json.loads(args.cls_mp)
+
+#print (directory)
+#print (data_directory)
+#print (class_weights)
+#print (class_maps)
 
 try:
     #os.mkdir(directory+'/labels/')
@@ -53,10 +55,16 @@ except OSError as error:
 
 def wt_frm_name(name):
   wt = class_weights[name]
-  #print(class_weights[name])
+  return wt
+
+def map_frm_name(name):
+  wt = class_maps[name]
   return wt
 
 for mode in ['training', 'test']:
+    inf_ct = 0
+    uninf_ct = 0
+    rmv = 0
     with open (data_directory + "/{}.json".format(mode), "r") as myfile:
         data = json.loads(myfile.read())
 
@@ -82,7 +90,7 @@ for mode in ['training', 'test']:
             output += "\n\t\t<depth>3</depth>\n\t</size>\n\t<segmented>0</segmented>"
 
             for object in sample['objects']:
-                category = object['category']
+                category = map_frm_name(object['category'])
                 #print(category)
                 xmin = object['bounding_box']['minimum']['c']
                 ymin = object['bounding_box']['minimum']['r']
@@ -90,17 +98,25 @@ for mode in ['training', 'test']:
                 ymax = object['bounding_box']['maximum']['r']
 
                 #https://stackoverflow.com/questions/51862997/class-weights-for-balancing-data-in-tensorflow-object-detection-api
-                #if category != 'red blood cell': # take out rbcs from annotation
-                output += "\n\t<object>\n\t\t<name>{}</name>\n\t\t<pose>Unspecified</pose>".format(category)
-               
-                output += "\n\t\t<truncated>0</truncated>\n\t\t<difficult>0</difficult>\n\t\t<bndbox>"
-                output += "\n\t\t\t<xmin>{}</xmin>\n\t\t\t<ymin>{}</ymin>".format(xmin, ymin)
-                output += "\n\t\t\t<xmax>{}</xmax>\n\t\t\t<ymax>{}</ymax>".format(xmax, ymax)
-                output += "\n\t\t</bndbox>"
-                output += "\n\t\t<weight>{}</weight>".format(wt_frm_name(category)) # to add class weights to annotation https://stackoverflow.com/questions/51862997/class-weights-for-balancing-data-in-tensorflow-object-detection-api
-                output += "\n\t</object>"
+                if category == 'infected': 
+                  inf_ct += 1
+                if category == 'uninfected':
+                  uninf_ct += 1
+                if category == 'rmv':
+                  rmv += 1
+                if category != 'rmv': # take out rbcs from annotation
+                  output += "\n\t<object>\n\t\t<name>{}</name>\n\t\t<pose>Unspecified</pose>".format(category)
+                
+                  output += "\n\t\t<truncated>0</truncated>\n\t\t<difficult>0</difficult>\n\t\t<bndbox>"
+                  output += "\n\t\t\t<xmin>{}</xmin>\n\t\t\t<ymin>{}</ymin>".format(xmin, ymin)
+                  output += "\n\t\t\t<xmax>{}</xmax>\n\t\t\t<ymax>{}</ymax>".format(xmax, ymax)
+                  output += "\n\t\t</bndbox>"
+                  output += "\n\t\t<weight>{}</weight>".format(wt_frm_name(category)) # to add class weights to annotation https://stackoverflow.com/questions/51862997/class-weights-for-balancing-data-in-tensorflow-object-detection-api
+                  output += "\n\t</object>"
 
             output += "\n</annotation>"
 
             with open(directory + "/{}/{}.xml".format(mode, sample['image']['pathname'].split('/')[-1].split('.')[0]), "w") as myfile:
                 myfile.write(output)
+
+    print ("{} dataset created and contains {} uninfected cells, {} infected cells. {} cells were removed.".format(mode, uninf_ct, inf_ct, rmv))
