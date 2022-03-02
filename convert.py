e5b7ef3447158b2d0f0f7db8df76060a77d49424
elvischido <96382131+elvischido@users.noreply.github.com>
2022-03-02 11:07:17 +1030

    Update convert.py

--- convert.py
+++ convert.py
@@ -112,7 +112,7 @@ for mode in ['training', 'test']:
                 filename = sample['image']['pathname'].split('/')[-1]
 
                 if mode == 'training':
-                    path = '..' + '/' + status + '/' + sample['image']['pathname']
+                    path = '..' + status + '/' + sample['image']['pathname']
                 else:
                     path = '..' + sample['image']['pathname']
 
